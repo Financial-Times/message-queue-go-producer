a97fdf4da610c90dd6f979b5c92b68f2a3f0a00c
Bernadette Varga <bernadette.varga@ft.com>
2016-02-01 11:29:04 +0200

    Add basic functionality for writing messages in kafka through the kafka proxy.

---
+++ producer/producer.go
@@ -0,0 +1,154 @@
+package producer
+
+import (
+	"encoding/json"
+	"encoding/base64"
+	"log"
+	"net/http"
+	"strings"
+	"errors"
+	"fmt"
+	"sort"
+)
+
+const CONTENT_TYPE_HEADER = "application/vnd.kafka.binary.v1+json"
+
+//Interface for message producer - which writes to kafka through the proxy
+type MessageProducer interface {
+	SendRawMessage(string, string) error
+	SendMessage(string, Message) error
+}
+
+type DefaultMessageProducer struct {
+	config MessageProducerConfig
+	client *http.Client
+}
+
+//Configuration for message producer
+type MessageProducerConfig struct {
+	//proxy address
+	Addr  string `json:"address"`
+	Topic string `json:"topic"`
+	//the name of the queue
+	//leave it empty for requests to UCS kafka-proxy
+	Queue string `json:"queue"`
+}
+
+//Message is the higher-level representation of messages from the queue: containing headers and message body
+type Message struct {
+	Headers map[string]string
+	Body    string
+}
+
+//Message format required by Kafka-Proxy containing all the Messages
+type MessageWithRecords struct {
+	Records []MessageRecord `json:"records"`
+}
+
+//Message format required by Kafka-Proxy
+type MessageRecord struct {
+	Key   string `json:"key"`
+	Value string `json:"value"`
+}
+
+// NewMessageProducer returns a producer instance
+func NewMessageProducer(config MessageProducerConfig) MessageProducer {
+	return &DefaultMessageProducer{config, &http.Client{}}
+}
+
+func (p *DefaultMessageProducer) SendMessage(uuid string, message Message) (err error) {
+
+	//concatenate message headers with message body to form a proper message string to save
+	messageString := buildMessage(message)
+	return p.SendRawMessage(uuid, messageString)
+}
+
+func (p *DefaultMessageProducer) SendRawMessage(uuid string, message string) (err error) {
+
+	//encode in base64 and envelope the message
+	envelopedMessage, err := envelopeMessage(uuid, message)
+	if err != nil {
+		return
+	}
+
+	//create request
+	req, err := constructRequest(p.config.Addr, p.config.Topic, p.config.Queue, envelopedMessage)
+
+	//make request
+	resp, err := p.client.Do(req)
+	if err != nil {
+		log.Printf("ERROR - executing request: %s", err.Error())
+		return
+	}
+	defer resp.Body.Close()
+
+	//send - verify response status
+	//log if error happens
+	if resp.StatusCode != http.StatusOK {
+		err = fmt.Errorf("Unexpected response status %d. Expected: %d.", resp.StatusCode, http.StatusOK)
+		log.Printf("ERROR - %s %s", err.Error(), resp.Request.URL.String())
+		return err
+	}
+
+	return nil
+}
+
+func constructRequest(addr string, topic string, queue string, message string) (*http.Request, error) {
+
+	req, err := http.NewRequest("POST", addr + "/topics/" + topic, strings.NewReader(message))
+	if err != nil {
+		log.Printf("ERROR - creating request: %s", err.Error())
+		return req, err
+	}
+
+	//set content-type header to json, and host header according to vulcand routing strategy
+	req.Header.Add("Content-Type", CONTENT_TYPE_HEADER)
+	req.Host = queue
+	return req, err
+}
+
+func buildMessage(message Message) (string) {
+
+	builtMessage := "FTMSG/1.0\n"
+
+	var keys []string
+
+	//order headers
+	for header := range message.Headers {
+		keys = append(keys, header)
+	}
+	sort.Strings(keys)
+
+	//set headers
+	for _, key := range keys {
+		builtMessage = builtMessage + key + ": " + message.Headers[key] + "\n"
+	}
+
+	builtMessage = builtMessage + "\n" + message.Body
+
+	return builtMessage
+
+}
+
+func envelopeMessage(key string, message string) (string, error) {
+
+	if key == "" {
+		log.Printf("Key cannot be empty. Please provide a valid UUID!")
+		return "", errors.New("Key cannot be empty. Please provide a valid UUID!")
+	}
+
+	key64 := base64.StdEncoding.EncodeToString([]byte(key))
+	message64 := base64.StdEncoding.EncodeToString([]byte(message))
+
+	record := MessageRecord{Key:key64, Value:message64}
+	msgWithRecords := &MessageWithRecords{Records:[]MessageRecord{record}}
+
+	jsonRecords, err := json.Marshal(msgWithRecords)
+
+	if err != nil {
+		log.Printf("ERROR - marshalling in json: %s", err.Error())
+		return "", err
+	}
+
+	return string(jsonRecords), err
+}
---
+++ producer/producer_test.go
@@ -0,0 +1,154 @@
+package producer
+
+import (
+	"io"
+	"testing"
+	"errors"
+	"github.com/golang/go/src/pkg/io/ioutil"
+)
+
+func TestBuildMessage(t *testing.T) {
+	var tests = []struct {
+		message      Message
+		builtMessage string
+	}{
+		{
+			Message{
+				map[string]string{
+					"Content-Type":      "application/json",
+					"Message-Id":        "c6653374-922c-4b78-927d-15c5125fcd8d",
+					"Message-Timestamp": "2015-10-21T14:22:06.270Z",
+					"Message-Type":      "cms-content-published",
+					"Origin-System-Id":  "http://cmdb.ft.com/systems/methode-web-pub",
+					"X-Request-Id":      "SYNTHETIC-REQ-MON_A391MMaVMv",
+				},
+				`{"contentUri":"http://methode-image-model-transformer-pr-uk-int.svc.ft.com/image/model/c94a3a57-3c99-423c-a6bd-ed8c4c10a3c3",
+"uuid":"c94a3a57-3c99-423c-a6bd-ed8c4c10a3c3", "destination":"methode-image-model-transformer", "relativeUrl":"/image/model/c94a3a57-3c99-423c-a6bd-ed8c4c10a3c3"}`,
+			},
+			`FTMSG/1.0
+Content-Type: application/json
+Message-Id: c6653374-922c-4b78-927d-15c5125fcd8d
+Message-Timestamp: 2015-10-21T14:22:06.270Z
+Message-Type: cms-content-published
+Origin-System-Id: http://cmdb.ft.com/systems/methode-web-pub
+X-Request-Id: SYNTHETIC-REQ-MON_A391MMaVMv
+
+{"contentUri":"http://methode-image-model-transformer-pr-uk-int.svc.ft.com/image/model/c94a3a57-3c99-423c-a6bd-ed8c4c10a3c3",
+"uuid":"c94a3a57-3c99-423c-a6bd-ed8c4c10a3c3", "destination":"methode-image-model-transformer", "relativeUrl":"/image/model/c94a3a57-3c99-423c-a6bd-ed8c4c10a3c3"}`,
+		},
+	}
+
+	for _, test := range tests {
+		resultingMessage := buildMessage(test.message)
+		if resultingMessage != test.builtMessage {
+			t.Errorf("Expected: msgs: %v\nActual: msgs: %v.",
+				test.builtMessage, resultingMessage)
+		}
+	}
+}
+
+func TestEnvelopeMessage(t *testing.T) {
+	var tests = []struct {
+		key              string
+		message          string
+		envelopedMessage string
+		err              error
+	}{
+		{
+			"3d91a94c-6ce6-4ec9-a16b-8b89be574ecc",
+			"a simple test message",
+			"{\"records\":[{\"key\":\"M2Q5MWE5NGMtNmNlNi00ZWM5LWExNmItOGI4OWJlNTc0ZWNj\",\"value\":\"YSBzaW1wbGUgdGVzdCBtZXNzYWdl\"}]}",
+			nil,
+		},
+		{
+			"",
+			"a simple test message",
+			"",
+			errors.New("Key cannot be empty. Please provide a valid UUID!"),
+		},
+		{
+			"3d91a94c-6ce6-4ec9-a16b-8b89be574ecc",
+			"",
+			"{\"records\":[{\"key\":\"M2Q5MWE5NGMtNmNlNi00ZWM5LWExNmItOGI4OWJlNTc0ZWNj\",\"value\":\"\"}]}",
+			nil,
+		},
+	}
+
+	for _, test := range tests {
+		resultingMessage, err := envelopeMessage(test.key, test.message)
+		if resultingMessage != test.envelopedMessage || (err != test.err && err.Error() != test.err.Error()) {
+			t.Errorf("Expected: msgs: %v, error: %v\nActual: msgs: %v, error: %v.",
+				test.envelopedMessage, test.err, resultingMessage, err)
+		}
+	}
+}
+
+func TestConstructRequest(t *testing.T) {
+	var tests = []struct {
+		addr               string
+		topic              string
+		queue              string
+		message            string
+		expectedRequestURL string
+		expectedError      error
+	}{
+		{
+			"https://localhost:8080",
+			"test",
+			"kafka-proxy",
+			"simple message",
+			"https://localhost:8080/topics/test",
+			nil,
+		},
+		{
+			"https://localhost:8080",
+			"test",
+			"kafka-proxy",
+			`FTMSG/1.0
+			Message-Id: c4b96810-03e8-4057-84c5-dcc3a8c61a26
+			Message-Timestamp: 2015-10-19T09:30:29.110Z
+			Message-Type: cms-content-published
+			Origin-System-Id: http://cmdb.ft.com/systems/methode-web-pub
+			Content-Type: application/json
+			X-Request-Id: SYNTHETIC-REQ-MON_Unv1K838lY
+
+			{"uuid":"e7a3b814-59ee-459e-8f60-517f3e80ed99", "value":"test","attributes":[]}`,
+			"https://localhost:8080/topics/test",
+			nil,
+		},
+	}
+
+	for _, test := range tests {
+		request, err := constructRequest(test.addr, test.topic, test.queue, test.message)
+		if request.URL.String() != test.expectedRequestURL {
+			t.Errorf("Expected: url: %v, \nActual: url: %v.",
+				test.expectedRequestURL, request.URL)
+		} else if request.Host != test.queue {
+			t.Errorf("Expected: host: %v, \nActual: host: %v.",
+				test.queue, request.Host)
+		} else if !containsMessage(request.Body, test.message) {
+			t.Errorf("Expected: message: %v, \nActual: message: %v.",
+				test.expectedRequestURL, request.URL)
+		} else if request.Header.Get("Content-Type") != CONTENT_TYPE_HEADER {
+			t.Errorf("Expected: Content-Type: %v\nActual: Content-Type: %v.",
+				CONTENT_TYPE_HEADER, request.Header.Get("Content-Type"))
+		} else if (err != test.expectedError && err.Error() != test.expectedError.Error()) {
+			t.Errorf("Expected: error: %v\nActual: error: %v.",
+				test.expectedError, err)
+		}
+	}
+}
+
+func containsMessage(body io.ReadCloser, message string) bool {
+	if body == nil {
+		return false
+	}
+	data, err := ioutil.ReadAll(body)
+	if err != nil {
+		return false
+	} else if string(data) == message {
+		return true
+	} else {
+		return false
+	}
+}
